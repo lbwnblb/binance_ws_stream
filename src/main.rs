dc7828f24f8052752a1b7ff422394a79186cf1b4
lbwnb <2781@>
2025-06-22 15:58:01 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use serde_json::json;
-use tungstenite::{connect};
+use tungstenite::{connect, Message, Utf8Bytes};
 fn main() {
-
+    let subscribe = json!({
+                "method": "SUBSCRIBE",
+                "params": ["btcusdt@trade"],
+                "id": 1
+            }).to_string();
     match connect("wss://stream.binance.com:9443/ws"){
         Ok((mut socket, response)) => {
-            println!("响应: {:?}", response)
+            if response.status().as_u16() == 101 {
+                match socket.send(Message::Text(Utf8Bytes::from(subscribe))){
+                    Ok(_) => {
+                        loop {
+                            match socket.read() {
+                                Ok(Message::Text(text)) => {
+                                    println!("{}", text);
+                                },
+                                Ok(_) => {},
+                                Err(_) => {}
+                            }
+                        }
+                    }
+                    Err(_) => {}
+                }
+            };
+
+
         },
         Err(e) => {
             println!("连接异常: {}", e);
